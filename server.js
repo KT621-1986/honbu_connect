c70b5497ae6f485808c7987044f7b4675544556c
tanaka_kouzaburo <sub.adderess.3.5360@gmail.com>
2026-05-13 15:39:12 +0900

    mobile responsive UI

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const { v4: uuidv4 } = require('uuid');
 const initSqlJs = require('sql.js');
 
 const app        = express();
-const PORT = process.env.PORT || 3000;
+const PORT       = process.env.PORT || 3000;
 const JWT_SECRET = 'honbu-connect-secret-2026';
 const DB_FILE    = path.join(__dirname, 'honbu_connect.db');
 
